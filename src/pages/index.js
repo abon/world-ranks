bd652aa3bb96de976c31ef14bcd630ece09f86f9
abon <formygithubacc@gmail.com>
2021-01-26 16:29:56 +0500

    initial commit with api configs:heavy_check_mark:

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout/Layout";
+
+export default function Home({ countries }) {
+  console.log(countries);
+  return <Layout>i am children</Layout>;
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
